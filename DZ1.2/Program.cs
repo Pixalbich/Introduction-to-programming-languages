f709faf296d50ac79245c74b6efb17f9b0627f2a
«Pixalbich» <Najomnik2@mail.ru>
2022-11-20 21:18:10 +0300

    помог с дз

--- DZ1.2/Program.cs
+++ DZ1.2/Program.cs
@@ -3,7 +3,9 @@
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
-Console.WriteLine("Данная программа сообщает какое из трех чисел самое больше");
+
+
+/* Console.WriteLine("Данная программа сообщает какое из трех чисел самое больше");
 Console.WriteLine("введите первое число");
 int num1= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите второе число");
@@ -24,4 +26,16 @@ int num3= Convert.ToInt32(Console.ReadLine());
         else
         Console.WriteLine(num3);
     }
-  
+   */
+Console.WriteLine("Введите число"); 
+int num1= Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число"); 
+int num2= Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число"); 
+int num3= Convert.ToInt32(Console.ReadLine()); 
+ int max = num1; 
+if (num1>max) max = num1; 
+if (num2>max) max = num2; 
+if (num3>max) max = num3; 
+
+Console.WriteLine("Максимальное число "+max); 
--- DZ1.4/Program.cs
+++ DZ1.4/Program.cs
@@ -1,36 +1,49 @@
-﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+﻿//  Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-5 -> 2, 4
-8 -> 2, 4, 6, 8 */
-/* Console.WriteLine("введите число, до которого нужно показать все четные числа.");
-int N = Convert.ToInt32(Console.ReadLine());
-int P =4;
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8 */
+// /Console.WriteLine("введите число, до которого нужно показать все четные числа.");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int P =4;
     
-    do 
-{
-    Console.Write(P-2+", ");
-    P=P+2;
-}
-    while (P<N-2);
-    Console.Write(P+"."); */
+//     do 
+// {
+//     Console.Write(P-2+", ");
+//     P=P+2;
+// }
+//     while (P<N-2);
+//     Console.Write(P+"."); 
+
+
 Console.WriteLine("введите число(больше 1), до которого нужно показать все четные числа.");
 int N = Convert.ToInt32(Console.ReadLine());
-int P =1;
- if (N<=1)
-  Console.Write("введите число больше 1");
- else{
-    do 
-    {   if (P % 2 == 0) 
-        if (P<N)
-        Console.Write(P+", ");
-    
-        else
-        Console.Write(" ");
-        P=P+1;
+int P = 1;
+if (N <= 1)
+    Console.Write("введите число больше 1");
+else
+{
+    do
+    {
+        if (P % 2 == 0)
+            if (P < N)
+                Console.Write(P + ", ");
+
+            else
+                Console.Write(" ");
+        P = P + 1;
     }
-    while (P<N-1);
-    if (P % 2 == 0) 
-    Console.Write(P+".");
+    while (P < N - 1);
+    if (P % 2 == 0)
+        Console.Write(P + ".");
     else
-    Console.Write(P+1+".");
- }
+        Console.Write(P + 1 + "."); 
+} 
+/* Console.WriteLine("Введите число"); 
+int num1= Convert.ToInt32(Console.ReadLine()); 
+int num2 = 0; 
+while (num2<num1-1) 
+{ 
+    num2+=2; 
+    Console.Write(num2);  
+    }
+ */
