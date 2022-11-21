58aa1c12ee374c13e494a200212d7bfb9ecea50c
«Pixalbich» <Najomnik2@mail.ru>
2022-11-21 13:53:12 +0300

    лрлелана работа на семинаре

--- Sem1Z2/Program.cs
+++ Sem1Z2/Program.cs
@@ -6,9 +6,16 @@ a = 25, b = 5 -> да
 a = 2, b = 10 -> нет 
 a = 9, b = -3 -> да 
 a = -3 b = 9 -> нет */
+Console.WriteLine("является ли первое число квадратом второго?");
 Console.WriteLine("введите первое число");
 int num1= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите второе число");
 int num2= Convert.ToInt32(Console.ReadLine());
-int sqt = num*num;
-Console.WriteLine("квадрат числа "+ num+ " равен "+sqt);
+if (num1==num2*num2)
+{
+Console.WriteLine(num1+ " является квадратом числа "+num2);
+}
+else
+{
+    Console.WriteLine(num1+ " не является квадратом числа "+num2);
+}
