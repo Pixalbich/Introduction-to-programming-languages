508a25812c63ef0b7f1cd364c0d51424daefa6ee
«Pixalbich» <Najomnik2@mail.ru>
2022-11-24 14:21:30 +0300

    Lection 4 complite

---
+++ Lection/L4Z3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+int Factorial (int n)
+{
+    //1!=1
+    //0!=1
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+System.Console.WriteLine(Factorial(3)); //1*2*3=6
+ */
+ 
+double Factorial (int n)  //int Factorial (int n)
+{
+    //1!=1
+    //0!=1
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i =1; i<40; i++)
+{
+System.Console.WriteLine($"{i}!={Factorial(i)}"); //1*2*3=6
+//System.Console.WriteLine(Factorial(i)); //1*2*3=6
+}
--- Seminar/S3Z5/Program.cs
+++ Seminar/S3Z5/Program.cs
@@ -1,1 +1,11 @@
 ﻿
+int N = new Random().Next(10000,100000);
+string num = N.ToString(); 
+int i = num.Length; 
+Console.WriteLine(num);
+Console.WriteLine(num[0]);
+Console.WriteLine(num[1]);
+Console.WriteLine(num[2]);
+Console.WriteLine(num[3]);
+Console.WriteLine(num[4]);
+Console.WriteLine("массив = "+i);
