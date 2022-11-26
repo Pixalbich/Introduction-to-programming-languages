a8cbd74270c349b699fbc964b4b97214cac9d8f8
«Pixalbich» <Najomnik2@mail.ru>
2022-11-26 20:41:29 +0300

    Homework 5 complite

---
+++ Homework/DZ5/DZ5.1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Уточните размер массива");
+int massize = Convert.ToInt32(Console.ReadLine());
+int [] RandomArray(int size) 
+{
+    int[] array = new int[size];
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        int Arra = (array[i]);
+        if (Arra%2==0){count++;} //знаю что не красиво, но всего 1 условие.
+    }
+    Console.WriteLine(count+" - количество четных элементов в массиве: ");
+    return array;
+}
+int[] array = RandomArray(massize);
+
+Console.Write(String.Join(", ",array));
+
+/* Console.WriteLine("Уточните размер массива");
+int massize = Convert.ToInt32(Console.ReadLine());
+int PosArray(int num) 
+{
+    int[] array = new int[num];
+    int count = 0;
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        int Arra = (array[i]);
+        if (Arra%2==0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count+" - количество четных элементов в массиве: ");
+    return count;
+}
+int array = PosArray(massize);
+
+Console.Write(array); */
---
+++ Homework/DZ5/DZ5.3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 38: Задайте массив вещественных чисел.  (Любое число +- и 0)
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = { 2.4,  76.6};
+
+double MaxMin(double result)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+        result = max - min;
+    }
+    return result;
+}
+Console.WriteLine(MaxMin(array[0]));
+
+
+
+/* double [] array =  {2,7,22,2.4,78.6} ;  
+
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+            // Console.Write("+"+max);
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+            // Console.Write("-"+min);
+        }
+        double result = max - min;   
+    }
+    Console.WriteLine(max-min);
+ */
