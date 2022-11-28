8e10b7a03cac0cb78d1a1ee030abef45d1e58347
«Pixalbich» <Najomnik2@mail.ru>
2022-11-29 00:00:20 +0300

    Homework 6 complite

---
+++ Homework/DZ6/DZ6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Данная программа сообщает сообщает, сколько чисел больше 0 вы ввели");
+Console.Write("Сколько чисел вы собираетесь вводить? ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите " + (i + 1) + " число:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Вы ввели числа " + String.Join(", ", array) + ".");
+if (count > 0)
+{
+    Console.WriteLine("Из них " + count + " больше нуля.");
+}
+else
+{
+    Console.WriteLine("Среди введенных чисел нет тех, что больше нуля.");
+}
+
